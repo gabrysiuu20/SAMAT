e1617a14eed5ff72373929fc885f649f5d7c17a3
wnuk.michal2115 <micwnu2@st.amu.edu.pl>
2024-05-31 21:37:17 +0200

    virustotal file analysis

--- samatFrontend/src/components/FirstVm.js
+++ samatFrontend/src/components/FirstVm.js
@@ -3,7 +3,7 @@ import './CssForVms.css'
 import { Screen } from '../vncLib'
 import sleepyAndroid from '../assets/sleepyAndroid.png'
 import React, { useState, useEffect, useRef } from "react";
-import { useNavigate, useBeforeUnload } from "react-router-dom";
+import { useNavigate, useBeforeUnload, isRouteErrorResponse } from "react-router-dom";
 
 
 export default function FirstVm({isPending}) {
@@ -12,6 +12,54 @@ export default function FirstVm({isPending}) {
     const vncScreenRef = useRef(null)
 
     const navigate = useNavigate();
+
+    const fileInput = document.createElement('input');
+    fileInput.type = 'file';
+    fileInput.id = 'fileInput';
+
+    document.body.appendChild(fileInput);
+
+    // LIMIT API - 4 ZAPYTANIA/MIN, 500 ZAPYTAŃ/DZIEŃ
+    function sendFile() {
+      const file = fileInput.files[0];
+
+      const formData = new FormData();
+      formData.append('file', file);
+    
+      fetch('https://www.virustotal.com/api/v3/files', {
+        method: 'POST',
+        body: formData,
+        headers: {
+        accept: 'application/json', 
+        'x-apikey': '977f090b832f3539b859ca70241c08e8ffe386ea45c399c15a0f0a33f6a844af'}
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('File uploaded successfully:', data);
+        console.log('ID:', data.data.id);
+        analiseID(data.data.id)
+      })
+      .catch(error => {
+        console.error('Error uploading file:', error);
+      });
+    }
+
+    function analiseID(id) {
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          'x-apikey': '977f090b832f3539b859ca70241c08e8ffe386ea45c399c15a0f0a33f6a844af'
+        }
+      };
+      
+      fetch('https://www.virustotal.com/api/v3/analyses/' + id, options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+
+    }
+
     
     const isValid = vncUrl => {
       if (!vncUrl.startsWith("ws://") && !vncUrl.startsWith("wss://")) {
@@ -46,6 +94,9 @@ export default function FirstVm({isPending}) {
         </OptButton>
         <OptButton onClick={() => setNetwork(false)}>
           Offline mode
+        </OptButton>
+        <OptButton onClick={() => sendFile()}>
+          VirusTotal Check
         </OptButton>
         
         <div className="startButtonPosition">
